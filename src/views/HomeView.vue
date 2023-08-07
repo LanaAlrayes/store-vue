803834bc2a876e4f8cdde9ba3ffd6431ccd6438c
LanaAlrayes <lanaalrayss@gmail.com>
2023-08-07 23:07:41 +0200

    add Sections to Home

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+   <hero-v />
+   <item-v />
+   <equipment-v />
+   <slide-v />
+  </div>
+</template>
+
+<script>
+import EquipmentV from '@/components/EquipmentV.vue'
+import HeroV from '@/components/HeroV.vue'
+import ItemV from '@/components/ItemV.vue'
+import SlideV from '@/components/SlideV.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    HeroV,
+    ItemV,
+    SlideV,
+    EquipmentV
+  }
+}
+</script>
